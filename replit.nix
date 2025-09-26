0b1e36e51054df0764022679e7a5e1dee52e1deb
Chantelle <chantellevanheer@gmail.com>
2025-09-26 02:26:14 +0000

    Post-Recovery checkpoint

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.nodejs_22          # Node.js runtime
-    pkgs.nodePackages.npm   # npm CLI
-    pkgs.git                # git client
-    pkgs.cacert             # SSL certs
-    # pkgs.stripe-cli       # optional: only if you want Stripe CLI available in shell
-  ];
-}
